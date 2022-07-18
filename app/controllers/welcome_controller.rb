f4812882118224fc67e3ae0e80a497f1178984ad
Ubuntu <ubuntu@ip-172-31-42-193.ec2.internal>
2022-07-18 02:03:10 +0000

    Adds ability to upload a CSV file. Does not store any data yet.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,17 @@
+require 'csv'
+
 class WelcomeController < ApplicationController
   def index
     puts "*****************************hello"
+  end
+  
+  def upload
+    puts "*****************************upload"
+   
+    csv_text = params[:attendees].read
+    csv = CSV.parse(csv_text, :headers => true)
+    csv.each do |row|
+      puts row
+    end
   end
 end
