0094c014d7c25d509b0c96112d4ace7dcbef4523
Ubuntu <ubuntu@ip-172-31-41-46.ec2.internal>
2022-08-11 00:18:15 +0000

    Updates algorithm with retries and logic to accomodate for later rounds where sets of entirely new pairs are harder to generate.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -16,58 +16,76 @@ class AssignmentsController < ApplicationController
     number_of_assignments_needed = [@assignments_session.num_tables, @male_attendees.count].min
     
     @assignments_session.num_rounds.times do |n|
-      people_to_match_for_round = []
-      assignments_for_round = []
-      males_already_paired = []
-      females_already_paired = []
-      minglers = []
+      need_to_rerun_pairing = true
+      number_of_reruns = 0
+      max_num_of_reruns = 10000
       
-      minglers_from_previous_round = @mingle_table_by_round[n-1]
-      if minglers_from_previous_round.present? && minglers_from_previous_round.length > 0
-        minglers_from_previous_round.each do |attendee_who_mingled_last_round|
-          if attendee_who_mingled_last_round.gender == "male"
-            @male_attendees = move_attendee_to_start_of_line(attendee_who_mingled_last_round, @male_attendees)
-          else
-            @female_attendees = move_attendee_to_start_of_line(attendee_who_mingled_last_round, @female_attendees)
+      while need_to_rerun_pairing && number_of_reruns < max_num_of_reruns
+        people_to_match_for_round = []
+        assignments_for_round = []
+        males_already_paired = []
+        females_already_paired = []
+        minglers = []
+        
+        minglers_from_previous_round = @mingle_table_by_round[n-1]
+        if minglers_from_previous_round.present? && minglers_from_previous_round.length > 0
+          minglers_from_previous_round.each do |attendee_who_mingled_last_round|
+            if attendee_who_mingled_last_round.gender == "male"
+              @male_attendees = move_attendee_to_start_of_line(attendee_who_mingled_last_round, @male_attendees)
+            else
+              @female_attendees = move_attendee_to_start_of_line(attendee_who_mingled_last_round, @female_attendees)
+            end
           end
         end
-      end
       
-      @male_attendees.slice(0..number_of_assignments_needed-1).each_with_index do |male_attendee, i|
-        @female_attendees.each do |female_attendee|
-          pairing_string = "#{male_attendee[:name]} + #{female_attendee[:name]}"
-          pairing_hash = {
-            male: male_attendee[:name],
-            female: female_attendee[:name]
-          }
+        @male_attendees.slice(0..number_of_assignments_needed-1).shuffle.each_with_index do |male_attendee, i|
+          @female_attendees.each do |female_attendee|
+            pairing_string = "#{male_attendee[:name]} + #{female_attendee[:name]}"
+            pairing_hash = {
+              male: male_attendee[:name],
+              female: female_attendee[:name]
+            }
+            
+            if (pairings_already_done.include?(pairing_string) && number_of_reruns != max_num_of_reruns) || females_already_paired.include?(female_attendee.id) || male_and_female_mingled_last_round?(male_attendee, female_attendee, minglers_from_previous_round)
+              next
+            else
+              assignments_for_round.push(pairing_hash)
+              
+              pairings_already_done.push(pairing_string)
+              
+              males_already_paired.push(male_attendee.id)
+              females_already_paired.push(female_attendee.id)
+              break
+            end
+          end
+        end
+        
+        if assignments_for_round.size == number_of_assignments_needed
+          need_to_rerun_pairing = false
           
-          if pairings_already_done.include?(pairing_string) || females_already_paired.include?(female_attendee.id)
-            next
-          else
-            assignments_for_round.push(pairing_hash)
-            
-            pairings_already_done.push(pairing_string)
-            
-            males_already_paired.push(male_attendee.id)
-            females_already_paired.push(female_attendee.id)
-            break
+          assignments_for_round = assignments_for_round.shuffle
+          
+          @pairings_by_round.push(assignments_for_round)
+          
+          @male_attendees.each do |male_attendee|
+            minglers.push(male_attendee) unless males_already_paired.include?(male_attendee.id)
           end
+          
+          @female_attendees.each do |female_attendee|
+            minglers.push(female_attendee) unless females_already_paired.include?(female_attendee.id)
+          end
+          
+          @mingle_table_by_round.push(minglers)
+        else
+          if (number_of_reruns > (max_num_of_reruns * 0.9).to_i)
+            pairings_already_done = pairings_already_done.drop(5)
+          end
+          
+          @male_attendees = @male_attendees.shuffle
+          @female_attendees = @female_attendees.shuffle
+          number_of_reruns = number_of_reruns + 1
         end
       end
-      
-      assignments_for_round = assignments_for_round.shuffle
-      
-      @pairings_by_round.push(assignments_for_round)
-      
-      @male_attendees.each do |male_attendee|
-        minglers.push(male_attendee) unless males_already_paired.include?(male_attendee.id)
-      end
-      
-      @female_attendees.each do |female_attendee|
-        minglers.push(female_attendee) unless females_already_paired.include?(female_attendee.id)
-      end
-      
-      @mingle_table_by_round.push(minglers)
     end
   end
   
@@ -75,6 +93,11 @@ class AssignmentsController < ApplicationController
   
   def move_attendee_to_start_of_line(attendee, attendee_array)
     original_array_without_attendee = attendee_array.filter {|a| a.id != attendee.id}
-    [attendee, original_array_without_attendee].flatten
+    [attendee, original_array_without_attendee.shuffle].flatten
+  end
+  
+  def male_and_female_mingled_last_round?(male, female, minglers_from_previous_round)
+    return false if minglers_from_previous_round.nil?
+    minglers_from_previous_round.include?(male) && minglers_from_previous_round.include?(female)
   end
 end
